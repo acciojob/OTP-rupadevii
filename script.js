bbb5ad64d26829845da7605dd683abca011bce0a
Rupa Devi <rupadevi7094@gmail.com>
2025-12-13 00:05:24 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ const container = document.querySelector(".code-container");
 for(let i=0; i<6; i++){
 	const inputEl = document.createElement("input");
 	inputEl.type = "number";
-	inputEl.id = i;
+	inputEl.id = `code-${i}`;
+	inputEl.dataset.id = i;
 	inputEl.className = "code";
 	inputEl.min = "0";
 	inputEl.max = "9";
@@ -13,12 +14,12 @@ for(let i=0; i<6; i++){
 	container.append(inputEl)
 }
 
-// container.children[0].focus();
+container.children[0].focus();
 
 function handleInput(e){
-	if(e.target.id <= 4 && e.inputType !== "deleteContentBackward") e.target.nextSibling.focus()
+	if(e.target.dataset.id <= 4 && e.inputType !== "deleteContentBackward") e.target.nextSibling.focus()
 
-	if(e.inputType === "deleteContentBackward" && e.target.id > 0) e.target.previousSibling.focus();
+	if(e.inputType === "deleteContentBackward" && e.target.dataset.id > 0) e.target.previousSibling.focus();
 	 
 }
 
